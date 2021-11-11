b088bed3059e3bc06ef24bf8b3b22305e701aada
Felipe Silva <felipe.silvads@outlook.com>
2021-11-11 20:30:40 -0300

    criado o controller do cardapio

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Participant\CardapioController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,6 +28,7 @@ Route::get('/estoque', function () {
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // ROTAS DO FELIPE SILVA
-
+//Rotas da pagina de cadastro de Cardapio
+Route::get('cardapio', [CardapioController::class, 'create'])->name('cadarpio.create');
 
 // ROTAS DO FELIPE MORAES
