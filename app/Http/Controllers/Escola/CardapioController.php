489780dd7be410e38e2a5d781ffa58ee36bfa4a1
Felipe Moraes <em.felipe@gmail.com>
2021-11-23 20:46:45 -0300

    correcao no controller create do cardapio

--- app/Http/Controllers/Escola/CardapioController.php
+++ app/Http/Controllers/Escola/CardapioController.php
@@ -11,7 +11,7 @@ class CardapioController extends Controller
 {
     public function create()
     {
-        return view('escola.cardapio.create');
+        return view('escola.cardapio');
     }
 
     public function store(CardapioRequest $request)
