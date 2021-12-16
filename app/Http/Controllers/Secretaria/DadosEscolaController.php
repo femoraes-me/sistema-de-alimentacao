ed8a343dadfdd4625695914d2ed7be18a884fe82
Anna Beatriz <anna.sjc@hotmail.com>
2021-12-15 21:03:51 -0300

    página de consumo diário secretaria

--- app/Http/Controllers/Secretaria/DadosEscolaController.php
+++ app/Http/Controllers/Secretaria/DadosEscolaController.php
@@ -7,6 +7,7 @@ use App\Models\Escola\Estoque;
 use Illuminate\Http\Request;
 use App\Models\Secretaria\Escola;
 use App\Models\Escola\Alimento;
+use App\Models\Escola\Consumo;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Models\Secretaria\Entrada;
@@ -21,10 +22,24 @@ class DadosEscolaController extends Controller
     }
 
 
-    public function exibeConsumo($id)
+    public function exibeConsumo(Request $request, $id)
     {
+
+        $data = $request->get('data') ?? Carbon::now()->format('Y-m-d');
+        // return $data;
         $escola = Escola::find($id);
-        return view('secretaria.escolas.consumo', compact('escola'));
+        $alimentos = DB::select(DB::raw('
+        select  
+            a.nome,
+            a.unidade,
+            sum(c.quantidade_consumida) quantidade_consumida
+        from consumos c 
+        inner join alimentos a on a.id = c.alimentos_id
+        where data = \'' . $data . '\'
+        group by a.nome, a.unidade'));
+
+        // return $alimentos;
+        return view('secretaria.escolas.consumo', compact('escola', 'alimentos', 'data'));
     }
 
     public function exibeCardapio($id)
--- resources/views/secretaria/escolas/consumo.blade.php
+++ resources/views/secretaria/escolas/consumo.blade.php
@@ -14,14 +14,17 @@
             <div class="d-flex justify-content-between p-4">
                 <div class="">
                     <label for="data_consumo">Dia:</label>
-                    <input type="date" name="data_consumo" id="data_consumo"
+                    <input type="date" name="data_consumo" id="data_consumo" value="{{ $data }}"
                         class="text-secondary border rounded p-1 {{ $errors->has('data_consumo') ? 'is-invalid' : '' }}">
-                    <button type="submit" class="btn letra btn-dark px-4"><i class="fa fa-search"></i>     Buscar</button>
+                    <button class="btn letra btn-dark px-4" onclick="location.search = `?data=${$('#data_consumo').val()}`;">
+                        <i class="fa fa-search"></i>
+                        Buscar
+                    </button>
                     <div class="invalid-feedback"></div>
                 </div>
                 <div class="">
-                    <button onclick="history.back()" class="btn letra btn-danger font-weight-bolder px-4"
-                        id="escolaCadastro">Voltar</button>
+                    <a href="{{ route('secretaria.escolas.actions', $escola->id) }}"
+                        class="btn letra btn-danger font-weight-bolder px-4" id="escolaCadastro">Voltar</a>
                 </div>
             </div>
         </div>
@@ -37,7 +40,13 @@
                             </tr>
                         </thead>
                         <tbody>
-                            
+                            @foreach ($alimentos as $alimento)
+                                <tr>
+                                    <td class="col-6">{{ $alimento->nome }}</td>
+                                    <td class="text-center col-3">{{ $alimento->unidade }}</td>
+                                    <td class="text-center  col-3">{{ $alimento->quantidade_consumida }}</td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
